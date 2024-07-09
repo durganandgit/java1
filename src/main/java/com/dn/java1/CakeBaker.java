3e3cb75793a4b45ea169e0e17bfa85f92e2f0961
durganand <durganandsantosh@gmail.com>
2024-07-09 21:35:45 +0530

    Class CakeBaker Modified

--- src/main/java/com/dn/java1/CakeBaker.java
+++ src/main/java/com/dn/java1/CakeBaker.java
@@ -1,4 +1,22 @@
 package com.dn.java1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CakeBaker {
+    @Autowired
+    private Frosting frosting;
+    @Autowired
+    private Syrup syrup;
+
+    String bakeCake(){
+        return frosting.getFrostingType()+" "+ syrup.getSyrupType();
+    }
 }
+
+
+
+
+
+
